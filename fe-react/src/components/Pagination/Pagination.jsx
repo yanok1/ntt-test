d3e1ad8e0622c2af42ef47942d79988d99c229cf
Carlos Yano <carlos.yano@newoxygen.com>
2024-01-15 15:32:31 -0300

    implementing Pagination component

---
+++ fe-react/src/components/Pagination/Pagination.jsx
@@ -0,0 +1,39 @@
+import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import { pageModifier } from "../../store/actions/movieActions";
+import { useSelector, useDispatch } from "react-redux";
+import "./Pagination";
+
+const IndexPagination = ({ numberPages }) => {
+  const page = useSelector((props) => props.page);
+  const dispatcher = useDispatch();
+  return (
+    <span className="index-pagination">
+      {Array.from({ length: numberPages }, (_, i) => (
+        <button
+          className={`basic-letter ${page ? "selected" : null}`}
+          key={i}
+          onClick={() => {
+            pageModifier(i + 1)(dispatcher);
+          }}
+        >
+          {i + 1}
+        </button>
+      ))}
+    </span>
+  );
+};
+
+const Pagination = ({ quantityPages }) => {
+  return (
+    <div className="pagination">
+      <IndexPagination numberPages={quantityPages}></IndexPagination>
+    </div>
+  );
+};
+
+Pagination.propTypes = {
+  quantityPages: PropTypes.number,
+};
+
+export default Pagination;
