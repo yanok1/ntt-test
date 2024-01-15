a51d8d8888b384f15528dc9cb9d5b933c1c6f197
Carlos Yano <carlos.yano@newoxygen.com>
2024-01-15 19:47:34 -0300

    styling searchbar, implementing reset and pressdown functionality

--- fe-react/src/components/SearchBar/SearchBar.jsx
+++ fe-react/src/components/SearchBar/SearchBar.jsx
@@ -1,25 +1,39 @@
-import { Button, Input, Icon } from "@ui5/webcomponents-react";
+import React, { useRef } from "react";
+import { Button, Input } from "@ui5/webcomponents-react";
+import "@ui5/webcomponents-icons/dist/search.js";
+import "@ui5/webcomponents-icons/dist/reset.js";
+import "./SearchBar.scss";
 
+const SearchBar = ({ query, setQuery, onSearch }) => {
+  const inputRef = useRef(null);
 
-const SearchBar = ({query,setQuery, onSearch}) => {
-  
-  return <div> 
-  <Input
-  icon={<Icon name="employee" />}
-  value={query}
-  onChange={(e)=>{
-    if(e.target.value){
-      setQuery(()=>e.target.value)
-    }
-  }}
-  onInput={function _a(){}}
-  onSuggestionItemPreview={function _a(){}}
-  onSuggestionItemSelect={function _a(){}}
-/>
-    <Button onClick={onSearch}> Search </Button>
-    <Button> Reset </Button>
-  </div>
-}
-
+  const eraseContent = () => {
+    setQuery("");
+    inputRef.current.focus();
+  };
+  return (
+    <div>
+      <Input
+        ref={inputRef}
+        className="searchInput"
+        value={query}
+        onChange={(e) => {
+          if (e.target.value) {
+            setQuery(() => e.target.value);
+          }
+        }}
+        onInput={function _a() {}}
+        onSuggestionItemPreview={function _a() {}}
+        onSuggestionItemSelect={function _a() {}}
+      />
+      <Button className="searchButton" onClick={onSearch}>
+        <ui5-icon name="search"></ui5-icon>
+      </Button>
+      <Button className="resetButton" onClick={eraseContent}>
+        <ui5-icon name="reset"></ui5-icon>
+      </Button>
+    </div>
+  );
+};
 
 export default SearchBar;
