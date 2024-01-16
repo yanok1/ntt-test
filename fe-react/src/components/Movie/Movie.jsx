0e9657b681aac643052d8a86297e8cbee1a5fdc4
Carlos Yano <carlos.yano@newoxygen.com>
2024-01-15 21:58:00 -0300

    styling movielist component

--- fe-react/src/components/Movie/Movie.jsx
+++ fe-react/src/components/Movie/Movie.jsx
@@ -1,10 +1,20 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { Title, Card, Text, Button, Label } from "@ui5/webcomponents-react";
+import {
+  Title,
+  Card,
+  Text,
+  Button,
+  Label,
+  List,
+  StandardListItem,
+  Icon,
+} from "@ui5/webcomponents-react";
 import "./Movie.scss";
 import Ratio from "../Ratio/Ratio";
 import "@ui5/webcomponents/dist/Icon"; // Import the SAP UI5 Icon
 import Spinner from "../Spinner/Spinner";
+import "@ui5/webcomponents-icons/dist/heart-2.js";
 
 const Movie = ({ id, actors, title, description, image, score }) => {
   const [disable, setDisable] = useState(false);
@@ -21,38 +31,46 @@ const Movie = ({ id, actors, title, description, image, score }) => {
 
   return (
     <section>
-      <div className="movie">
-        <Card heading="Sample Card" subtitle="Subtitle">
-          <Title className="title">{title}</Title>
-          <Text>{description}</Text>
-          <div className="label color-black-default default-gap">
-            <Label className="bold color-black-default">Actor</Label>
-            <span>{actors}</span>
+      <div className="movie-wrapper">
+        <article className="movie">
+          <div className="wrapper-content">
+            <Title className="title">{title}</Title>
+            <Text className="align-start">{description}</Text>
+            <div className="label color-black-default default-gap align-start">
+              <Label className="bold color-black-default">Actor</Label>
+              <span>{actors}</span>
+            </div>
+            <div className="label color-black-default start-gap align-start">
+              <Label className="bold color-black-default">Review</Label>
+              <Ratio score={score}></Ratio>
+            </div>
+            <Button
+              disabled={disable}
+              className="align-start"
+              onClick={(e) => {
+                localStorage.setItem(id, JSON.stringify({ favorite: true }));
+                setDisable(true);
+              }}
+            >
+              <span className="btn-display">
+                Favorite
+                <ui5-icon name="heart-2"></ui5-icon>
+              </span>
+            </Button>
           </div>
-          <div className="label color-black-default start-gap">
-            <Label className="bold color-black-default">Review</Label>
-            <Ratio score={score}></Ratio>
+          <div className={`wrapper-image `}>
+            {loading ? <Spinner isLoading={loading}></Spinner> : null}
+            <img
+              className={`image ${loading ? `display-none not-render` : null}`}
+              src={image}
+              alt={title}
+              loading="lazy"
+              onLoad={() => {
+                setLoading(() => false);
+              }}
+            />
           </div>
-          <Button
-            disabled={disable}
-            icon="employee"
-            onClick={() => {
-              localStorage.setItem(id, JSON.stringify({ favorite: true }));
-            }}
-          >
-            Favorite
-          </Button>
-        </Card>
-        {loading ? <Spinner isLoading={loading}></Spinner> : null}
-        <img
-          className={loading ? `display-none` : null}
-          src={image}
-          alt={title}
-          loading="lazy"
-          onLoad={() => {
-            setLoading(() => false);
-          }}
-        />
+        </article>
       </div>
     </section>
   );
